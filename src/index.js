152a471c9a7e6311025c7b556b0f57b1eeace5d8
Leonor <leonorcascais@hotmail.com>
2022-02-15 15:56:44 +0100

    Create React App and clean it

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return (
+        <div>Welcome to ReadMe</div>
+    )
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));

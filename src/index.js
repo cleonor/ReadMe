72a643de117c2d469901b36dab68b04b6f8dd750
Leonor <leonorcascais@hotmail.com>
2022-02-15 19:10:03 +0100

    handle user input through useState hook

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from "react";
 import ReactDOM from 'react-dom';
 
 const App = () => {
+
+    const [numPages, setNumPages] = useState();
+
     return (
-        <div>Welcome to ReadMe</div>
+        <div><h1>Welcome to ReadMe</h1>
+            <div>
+                <label>
+                    Number of pages:
+                </label>
+                <input
+                    type="text"
+                    value={numPages}
+                    onInput={e => setNumPages(e.target.value)}
+                />
+            </div>
+        </div>
     )
 }
 

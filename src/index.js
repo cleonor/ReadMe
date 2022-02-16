63547f9738bd7714446cec8e5470c839c7d3e60b
Leonor <leonorcascais@hotmail.com>
2022-02-16 20:27:39 +0100

    Input for title

--- src/index.js
+++ src/index.js
@@ -4,18 +4,31 @@ import ReactDOM from 'react-dom';
 const App = () => {
 
     const [numPages, setNumPages] = useState();
+    const [title, setTitle] = useState();
 
     return (
         <div><h1>Welcome to ReadMe</h1>
             <div>
-                <label>
-                    Number of pages:
-                </label>
-                <input
-                    type="text"
-                    value={numPages}
-                    onInput={e => setNumPages(e.target.value)}
-                />
+                <div>
+                    <label>
+                        Name of the book:
+                    </label>
+                    <input
+                        type="text"
+                        value={title}
+                        onInput={e => setTitle(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <label>
+                        Number of pages:
+                    </label>
+                    <input
+                        type="text"
+                        value={numPages}
+                        onInput={e => setNumPages(e.target.value)}
+                    />
+                </div>
             </div>
         </div>
     )

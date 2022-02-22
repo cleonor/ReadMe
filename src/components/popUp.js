551f9f1ee27be65e4084cd91b40dee67b8097760
Leonor <leonorcascais@hotmail.com>
2022-02-22 15:59:50 +0100

    import css file

--- src/components/popUp.js
+++ src/components/popUp.js
@@ -1,11 +1,12 @@
 import React from "react";
+import "./popUp.css"
 
 function PopUp(props) {
     return (props.trigger) ? (
         <div className="popup">
             <div className="popup-inner">
-                <butto>Back</butto>
-                <button>Continue</button>
+                <button className="backButton">Back</button>
+                <button className="continueButton">Continue</button>
                 {props.children}
             </div>
         </div>

f8f3fc1b378e94c870396b4cf6f4d84b883dd3e4
Leonor <leonorcascais@hotmail.com>
2022-03-07 19:43:50 +0100

    hardcode user name

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,17 @@
 const express = require('express')
+var cors = require('cors');
 const app = express()
 const port = 3001
 
+app.use(cors());
+
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
+})
+
+app.get('/user', (req, res) => {
+    res.send('Hello, Leonor')
 })
 
 app.listen(port, () => {

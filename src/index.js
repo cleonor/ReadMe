daadc4fac83dfb04af20976b8ac8ba3b6d59ab4c
Leonor <leonorcascais@hotmail.com>
2022-04-30 22:36:16 +0200

    apply mui styles to container

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ import PopUp from "./components/popUp";
 import "./index.css";
 import Navbar from "./components/navBar";
 import axios from "axios";
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
 const pushToBE = (title, numPages) => {
     axios.post('http://localhost:3001/newbook', {
@@ -35,24 +37,29 @@ const App = () => {
                     onSubmit={handleSubmit((data) => {
                         console.log(data);
                     })}>
-                    <div>
+                    <div className="bookInformation">
                         <label>
-                            Name of the book:
+                            Title of the book:
                         </label>
-                        <input
-                            type="text"
+                        <TextField
+                            required
+                            id="standard-required"
+                            variant="standard" type="text"
                             {...register("bookName", { required: "This is required." })}
                             value={title}
                             onInput={e => setTitle(e.target.value)}
                         />
                         <p>{errors.bookName?.message}</p>
-                    </div>
-                    <div>
                         <label>
                             Number of pages:
                         </label>
-                        <input
+                        <TextField
+                            id="standard-number"
                             type="number"
+                            InputLabelProps={{
+                                shrink: true,
+                            }}
+                            variant="standard"
                             {...register("numberPages", {
                                 required: "This is required.",
                                 min: {
@@ -96,29 +103,31 @@ const App = () => {
                             {date[0].toDateString()}
                         </div>
                     )}
-                    <input
+                    <Button variant="contained"
                         type="submit"
+                        color="inherit"
                         onClick={() => {
                             setTrigged(true);
                             pushToBE(title, numPages);
                         }}
-                    />
+                    >Submit</Button>
 
                 </form>
             </div>
-            {date.length > 1 && (<PopUp
-                trigger={istrigged}
-                setTrigged={setTrigged}
-            >
-                <h3>Title of the Book: {title}</h3>
-                <p>Number of Pages: {numPages}</p>
-                <p>Starting Date: {date[0].toDateString()}</p>
-                <p>End Date: {date[1].toDateString()}</p>
-            </PopUp>
-            )
+            {
+                date.length > 1 && (<PopUp
+                    trigger={istrigged}
+                    setTrigged={setTrigged}
+                >
+                    <h3>Title of the Book: {title}</h3>
+                    <p>Number of Pages: {numPages}</p>
+                    <p>Starting Date: {date[0].toDateString()}</p>
+                    <p>End Date: {date[1].toDateString()}</p>
+                </PopUp>
+                )
             }
 
-        </div>
+        </div >
     )
 }
 
